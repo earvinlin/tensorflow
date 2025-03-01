321abc7ad5727635e56363d3152af48248c15425
Earvin Lin(ASUSWinPC) <lin.earvin@gmail.com(ASUSWinPC)>
2025-03-01 19:16:20 +0800

    20250301-1916 add code.

--- scripts/BOOK_DLWP2018/py/ch02_from_dlwp.py
+++ scripts/BOOK_DLWP2018/py/ch02_from_dlwp.py
@@ -1,3 +1,11 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
+
 ###--- § 2-1 加載Keras中的MNIST數據集 ---###
 from keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -24,14 +32,27 @@ network.compile(optimizer='rmsprop',
 train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype('float32') / 255
 
-test_images = test_images.reshape((60000, 28 * 28))
+test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
 
 ###--- § 2-5 准備標籤 ---###
+"""
+to_categorical : 
+    Keras 庫中的一個便捷函數，用於將類別標籤轉換為 one-hot 編碼的格式。
+    在深度學習中，尤其是在分類任務中，這種格式對於訓練神經網絡非常有用。
+"""
 from keras.utils import to_categorical
 
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
+
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print("test_loss= ", test_loss, ", test_acc= ", test_acc)
+
+
+
 
 
 
