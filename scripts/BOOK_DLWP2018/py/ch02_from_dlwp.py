81a1a51d9df49655cb293be8667d0742700b35b2
lin.earvin(hpenvynb) <lin.earvin(hpenvbnb)>
2025-03-11 14:31:04 +0800

    20250311-1431 add code.

--- scripts/BOOK_DLWP2018/py/ch02_from_dlwp.py
+++ scripts/BOOK_DLWP2018/py/ch02_from_dlwp.py
@@ -84,6 +84,107 @@ plt.imshow(my_slice[4], cmap=plt.cm.binary)
 plt.show()
 """
 
+#################
+# §2.3.1 (p.30) #
+#################
+def naive_relu(x) :
+    assert len(x.shape) == 2
+
+    x = x.copy()
+    for i in range(x.shape[0]) :
+        for j in range(x.shape[1]) :
+            x[i, j] = max(x[i, j], 0)
+    return x
+
+a0 = np.array([[1,-3,3], [-2,5,7]])
+print("a0= ", naive_relu(a0))
+
+
+
+#################
+# §2.3.2 (p.31) #
+#################
+def naive_add_matrix_and_vector(x, y) :
+    assert len(x.shape) == 2
+    assert len(y.shape) == 1
+    assert x.shape[1] == y.shape[0]
+
+    x = x.copy()
+    for i in range(x.shape[0]) :
+        for j in range(x.shape[1]) :
+            x[i, j] += y[j]
+    return x
+
+a = np.array([[1,12,3], [2,3,9]])
+b = np.array([5,6,7])
+print("a.shape= ", a.shape)
+print("b.shape= ", b.shape)
+
+print("len(a.shape)= ", len(a.shape))
+print("len(b.shape)= ", len(b.shape))
+print("z= ", np.maximum(a,b))
+
+print("x= ", naive_add_matrix_and_vector(a,b))
+
+
+#################
+# §2.3.3 (p.32) #
+#################
+def naive_vector_dot(x, y) :
+    assert len(x.shape) == 1
+    assert len(y.shape) == 1
+    assert x.shape[0] == y.shape[0]
+
+    z = 0.
+    for i in range(x.shape[0]) :
+        z += x[i] * y[i]
+    return z
+
+a1 = np.array([1,3,2])
+a2 = np.array([2,5,-1])
+
+print("z2= ", naive_vector_dot(a1, a2))
+
+
+#################
+# §2.3.3 (p.32) #
+#################
+def naive_matrix_vector_dot(x, y) :
+    assert len(x.shape) == 2
+    assert len(y.shape) == 1
+    assert x.shape[1] == y.shape[0]
+
+    z = np.zeros(x.shape[0])
+    for i in range(x.shape[0]) :
+        for j in range(x.shape[1]) :
+            z[i] += x[i, j] * y[j]
+    return z
+
+def naive_matrix_vector_dot(x, y) :
+    z = np.zeros(x.shape[0])
+    for i in range(x.shape[0]) :
+        z[i] = naive_vector_dot(x[i,:], y)
+    return z
+
+def naive_matrix_dot(x, y) :
+    assert len(x.shape) == 2
+    assert len(y.shape) == 2
+    assert x.shape[1] == y.shape[0]
+
+    z = np.zeros((x.shape[0], y.shape[1]))
+    for i in range(x.shape[0]) :
+        for j in range(y.shape[1]) :
+            row_x = x[i, :]
+            column_y = y[:, j]
+            z[i, j] = naive_vector_dot(row_x, column_y)
+    return z
+
+
+
+
+
+
+
 
 
 
