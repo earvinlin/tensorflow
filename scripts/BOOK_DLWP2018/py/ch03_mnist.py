554d33d940ad1fe36cc2fe639691f76a596be9f3
earvinlin.02(imac) <earvinlin.02@gmail.com(imac)>
2025-03-25 08:42:51 +0800

    20250325-0842 add code.

--- scripts/BOOK_DLWP2018/py/ch03_mnist.py
+++ scripts/BOOK_DLWP2018/py/ch03_mnist.py
@@ -29,7 +29,85 @@ def vectorize_sequences(sequences, dimension=10000) :
     return results
 
 x_train = vectorize_sequences(train_data) # 將訓練數據向量化
-x_test = vectorize_sequcences(test_data)  # 將測試數據向量化
+x_test = vectorize_sequences(test_data)  # 將測試數據向量化
+
+def to_one_hot(labels, dimension=46) :
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels) :
+        results[i, label] = 1.
+    return results
+
+# Use one hot coding
+from keras.utils.np_utils import to_categorical
+
+one_hot_train_labels = to_categorical(train_labels)
+one_hot_test_labels = to_categorical(test_labels)
+
+print("train_labels(one hot)= ", one_hot_train_labels)
+print("test_labels(ont hot) = ", one_hot_test_labels)
+print("test_data[1]= ", test_data[1])
+print("test_data[][]= ", test_data[1][1])
+print("test_labels[][]= ", x_test[1][2768])
+
+# 3-15 模型定義
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+# 3-16 編譯模型
+model.compile(optimizer='rmsprop', 
+    loss='categorical_crossentropy',
+    metrics=['accuracy'])
+
+# 3-17 留出驗證集
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+# 3-18 訓練模型
+history = model.fit(partial_x_train,
+    partial_y_train,
+    epochs=20,
+    batch_size=512,
+    validation_data=(x_val, y_val))
+
+# 3-19 繪製訓練損失和驗證損失
+import matplotlib.pyplot as plt
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+# 3-20 繪製訓練精度和驗證精度
+plt.clf()
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, 'bo', label='Trainging acc')
+plt.plot(epochs, val_acc, 'b', label= 'Validation acc')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
+
+
 
 
 
