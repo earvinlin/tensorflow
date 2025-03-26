4386b259c801077d6c8d77895744d1900364aeb0
lin.earvin(hpenvynb) <lin.earvin(hpenvbnb)>
2025-03-26 22:43:20 +0800

    20250326-2243 add code.

--- scripts/BOOK_DLWP2018/py/ch03_reuters.py
+++ scripts/BOOK_DLWP2018/py/ch03_reuters.py
@@ -1,6 +1,11 @@
 from tensorflow import keras
 from keras.datasets import reuters
 
+# HPNB不加此段無法顯示圖形
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
 # 3-12
 (train_data, train_labels), (test_data, test_labels) = \
     reuters.load_data(num_words=10000)
@@ -14,13 +19,91 @@ print("train_data[10]= ", train_data[10])
 # 3-13
 word_index = reuters.get_word_index()
 reverse_word_index = dict([(value, key) \
-    for (key, value) in word_index_items()])
+    for (key, value) in word_index.items()])
 decoded_newswire = ' '.join([reverse_word_index.get(i - 3, '?') 
     for i in train_data[0]])
 
 print("train_labels[10]= ", train_labels[10])
 
+# 3-14 編碼數據
+import numpy as np
 
+def vectorize_sequences(sequences, dimension=10000) :
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences) :
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+def to_one_hot(labels, dimension=46) :
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels) :
+        results[i, label] = 1.
+    return results
+
+one_hot_train_labels = to_one_hot(train_labels)
+one_hot_test_labels = to_one_hot(test_labels)
+
+# 3-15 模型定義
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+# 3-16 編譯模型
+model.compile(optimizer='rmsprop', 
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 3-17 留出驗證集
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+# 3-18 訓練模型
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+# 3-19 繪製訓練損失和驗證損失
+import matplotlib.pyplot as plt
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+# 3-20 繪製訓練精度和驗證精度
+plt.clf()
+
+acc =history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
 
 
 
