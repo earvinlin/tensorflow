e339a72173d4164b617e860e8ff056f82588b470
earvinlin.02(imac) <earvinlin.02@gmail.com(imac)>
2025-04-30 08:53:32 +0800

    20250430-0853 add codes.

---
+++ scripts/BOOK_DLI16/ch08-intermediate_net_in_keras.py
@@ -0,0 +1,44 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import optimizers
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784).astype('float32')
+x_test = x_test.reshape(10000, 784).astype('float32')
+
+x_train /= 255
+x_test /= 255
+
+n_classes = 10
+y_train = to_categorical(y_train, n_classes)
+y_test = to_categorical(y_test, n_classes)
+
+# CH05
+#model = Sequential()
+#model.add(Dense(64, activation='sigmoid', input_shape=(784,)))
+#model.add(Dense(10, activation='softmax'))
+
+# CH08
+model = Sequential()
+model.add(Dense(64, activation='relu', input_shape=(784,)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+        optimizer=optimizers.SGD(learning_rate=0.01),
+        metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+        batch_size=128, epochs=20,
+        verbose=1,
+        validation_data=(x_test, y_test))
+
+
