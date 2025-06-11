a61d570bec63966fe07625a6f0b89fe9afefb79d
林俊吉 <earvin@linjunjideMacBook-Pro.local>
2025-06-11 22:09:19 +0800

    20250611-2209 add files.

--- scripts/BOOK_EasyStudyTF2/estf2_ch01-1.6.2.py
+++ scripts/BOOK_EasyStudyTF2/estf2_ch01-1.6.2.py
@@ -10,10 +10,14 @@ from keras import layers
 from keras.utils import plot_model
 from IPython.display import Image
 
-# Method 1
 model = keras.Sequential(name='Sequential')
+
+"""
+# [ Method 1 ]
+# 202506011 tensorflow 2.18.1 Input需要改寫如下方式
 #model.add(layers.Dense(64, activation='relu', input_shape=(784,)))
 model.add(layers.Input(shape=(784,)))
+
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
@@ -21,18 +25,18 @@ model.add(layers.Dense(10, activation='softmax'))
 model.compile(optimizer='rmsprop',
         loss='binary_crossentropy',
         metrics=['accuracy'])
-
-# Method 2
 """
+        
+# [ Method 2 ]
 model = tf.keras.Sequential([
-        layers.Dense(64, activation='relu', input_shape=(784,)),
+        layers.Input(shape=(784,)),
+        layers.Dense(64, activation='relu'),
         layers.Dense(64, activation='relu'),
         layers.Dense(10, activation='softmax')])
-"""
 
-plot_model(model, to_file='Functional_API_Sequential_model.png')
+plot_model(model, to_file='Functional_API_Sequential_Model.png')
 
-Image('Functional_API_Sequential_model.png')
+Image('Functional_API_Sequential_Model.png')
 
 """
 #img = Image.open('Functional_API_Sequential_model.png')
--- scripts/BOOK_EasyStudyTF2/readme.txt.txt
+++
@@ -1,3 +0,0 @@
-輕鬆學會TensorFlow 2.0 §3.3.1 <pdf.98>
-download-addr : https://www.kaggle.com/datasets/terminus7/pokemon-challenge
-
