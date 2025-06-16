466483d5aef84abc4bcb4656dbccc24e48d1d34e
earvinlin.02(mac14pronb) <earvinlin.02@gmail.com>
2025-06-16 20:44:47 +0800

    20250616-2044 add files.

--- scripts/BOOK_EasyStudyTF2/estf2_ch02-2.3.3_j.py
+++ scripts/BOOK_EasyStudyTF2/estf2_ch02-2.3.3_j.py
@@ -1,5 +1,5 @@
 """
-estf2_ch02-2.3.3.py
+estf2_ch02-2.3.3_j.py
 """
 
 import os
@@ -9,9 +9,106 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 from tensorflow import keras
-from tensorflow.keras import layers
+#from tensorflow.keras import layers
+from keras import layers
 
-data = pd.read_csv("kc_house_data.csv")
+data = pd.read_csv("data/kc_house_data.csv")
 # 顯示dataset的形狀，共21613比資料，每一比資料有21種不同資訊。
-data.shape
+print(data.shape)
 
+# 將顯示列數設定為25，不然會有部份資料無法顯示
+pd.options.display.max_columns = 25
+# head 會顯示前五行的數據
+print(data.head())
+
+print(data.dtypes)
+
+# 將date日期拆為年、月和日並轉成數值
+data['year'] = pd.to_numeric(data['date'].str.slice(0, 4))
+data['month'] = pd.to_numeric(data['date'].str.slice(4, 6))
+data['day'] = pd.to_numeric(data['date'].str.slice(6, 8))
+
+# 刪除沒有用的數據，inplace則是將更新後的資料存回原本的地方
+data.drop(['id'], axis="columns", inplace=True)
+data.drop(['date'], axis="columns", inplace=True)
+
+print(data.head())
+
+data_num = data.shape[0]
+# 取得一筆與data數量相同的亂數索引，主要目的是用於打散資料
+indexes = np.random.permutation(data_num)
+# 並將亂數索引值分為Train、validation和test分為，這裡的劃分比例為6:2:2
+train_indexes = indexes[:int(data_num *0.6)]
+val_indexes = indexes[int(data_num *0.6):int(data_num *0.8)]
+test_indexes = indexes[int(data_num *0.8):]
+# 透過索引值從data取出訓練資料、驗證資料和測試資料
+train_data = data.loc[train_indexes]
+val_data = data.loc[val_indexes]
+test_data = data.loc[test_indexes]
+
+train_validation_data = pd.concat([train_data, val_data])
+mean = train_validation_data.mean()
+std = train_validation_data.std()
+
+train_data = (train_data - mean) / std
+val_data = (val_data - mean) / std
+
+x_train = np.array(train_data.drop('price', axis='columns'))
+y_train = np.array(train_data['price'])
+x_val = np.array(val_data.drop('price', axis='columns'))
+y_val = np.array(val_data['price'])
+
+print(x_train.shape)
+
+# 建立一個Sequential型態的model
+model = keras.Sequential(name='model-1')
+# 第1層全連接層設為64個unit，將輸入形狀設定為(21, )，而實際上我們輸入的數據形狀為(batch_size, 21)
+model.add(layers.Dense(64, activation='relu', input_shape=(21,)))
+# 第2層全連接層設為64個unit
+model.add(layers.Dense(64, activation='relu'))
+# 最後一層全連接層設為1個unit
+model.add(layers.Dense(1))
+# 顯示網路模型架構
+model.summary()
+
+model.compile(keras.optimizers.Adam(0.001),
+              loss=keras.losses.MeanSquaredError(),
+              metrics=[keras.metrics.MeanAbsoluteError()])
+
+model_dir = 'lab2-logs/models/'
+os.makedirs(model_dir, exist_ok=True)
+
+# TensorBoard回調函數會幫忙紀錄訓練資訊，並存成TensorBoard的紀錄檔
+log_dir = os.path.join('lab2-logs', 'model-1')
+model_cbk = keras.callbacks.TensorBoard(log_dir=log_dir)
+# ModelCheckpoint回調函數幫忙儲存網路模型，可以設定只儲存最好的模型，「monitor」表示被監測的數據，「mode」min則代表監測數據越小越好。
+model_mckp = keras.callbacks.ModelCheckpoint(model_dir + '/Best-model-1.keras', 
+                                        monitor='val_mean_absolute_error', 
+                                        save_best_only=True, 
+                                        mode='min')
+
+history = model.fit(x_train, y_train,  # 傳入訓練數據
+               batch_size=64,  # 批次大小設為64
+               epochs=300,  # 整個dataset訓練300遍
+               validation_data=(x_val, y_val),  # 驗證數據
+               callbacks=[model_cbk, model_mckp])  # Tensorboard回調函數紀錄訓練過程，ModelCheckpoint回調函數儲存最好的模型
+
+print(history.history.keys())  # 查看history儲存的資訊有哪些
+
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.ylim(0.02, 0.2)
+plt.title('Mean square error')
+plt.ylabel('loss')
+plt.xlabel('epochs')
+plt.legend(loc='upper right')
+plt.show()
+
+plt.plot(history.history['mean_absolute_error'], label='train')
+plt.plot(history.history['val_mean_absolute_error'], label='validation')
+plt.ylim(0.12, 0.26)
+plt.title('Mean absolute error')
+plt.ylabel('metrics')
+plt.xlabel('epochs')
+plt.legend(loc='upper right')
+plt.show()
